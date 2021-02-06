69897c37c23d055c05537bc06e88ea89280a4334
scott-mckool <hydrogen.poo@gmail.com>
2021-02-06 13:50:58 -0500

    made playback Auton actually start a playback

--- src/main/java/frc/robot/commands/Drive_Commands/PlaybackAuton.java
+++ src/main/java/frc/robot/commands/Drive_Commands/PlaybackAuton.java
@@ -29,7 +29,7 @@ public class PlaybackAuton extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
+    m_Recorder.startPlayback();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
