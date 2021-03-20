dd00842dcfff7253bfee5caaafd0bff96f8e1057
scott-mckool <hydrogen.poo@gmail.com>
2021-03-20 15:55:41 -0400

    fixed var names

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,8 +16,8 @@ public final class Constants {
 
   public static final class RobotRecorderConstants{
     
-    public static final double  RECORDING_FREQUENCY   = 15;     // miliseconds (maybe change to microseconds)
-    public static final double  AUTONOMOUS_DURATION   = 5;      // seconds
+    public static final double  UPDATE_FREQUENCY      = 15;     // miliseconds (maybe change to microseconds)
+    public static final double  RECORDING_DURATION    = 5;      // seconds
     public static final String  SAVE_FILE_EXTENSION   = ".lmao";// extension for files that robotArrays are saved in
     public static final String  SAVE_FILE_PATH        = "/home/lvuser/";// path on the roborio to keep robotArray files( thanks for ruining the cool looking code )
     public static final String  SAVE_FILE_NAME        = "test"; // name of the file to make or read
