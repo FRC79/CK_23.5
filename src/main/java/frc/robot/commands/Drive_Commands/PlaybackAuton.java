896050c604a0d90b533013207a4475b5c62cf077
scott-mckool <hydrogen.poo@gmail.com>
2021-03-21 00:05:28 -0400

    added PID correction

--- src/main/java/frc/robot/commands/Drive_Commands/PlaybackAuton.java
+++ src/main/java/frc/robot/commands/Drive_Commands/PlaybackAuton.java
@@ -7,6 +7,7 @@ package frc.robot.commands.Drive_Commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotRecorder;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.Constants.DriveConstants;;
 
 public class PlaybackAuton extends CommandBase {
 
@@ -14,8 +15,13 @@ public class PlaybackAuton extends CommandBase {
   private DriveTrain m_DriveTrain;
 
   // values retrieved from robotRecorder
-  private double joyX;
-  private double joyY;
+  private Double joyX;
+  private Double joyY;
+  private Double leftEncoderDistRecorded;
+  private Double rightEncoderDistRecorded;
+  private double proportionWeight;
+  private double derivativeWeight;
+  private double PIDWeight;
   
 
   /** Creates a new PlaybackAuton. */
@@ -30,35 +36,56 @@ public class PlaybackAuton extends CommandBase {
   @Override
   public void initialize() {
     m_Recorder.startPlayback();
+    m_DriveTrain.resetEncoders();
+    PIDWeight = DriveConstants.PID_WEIGHT;
+    proportionWeight = DriveConstants.PROPORTION_WEIGHT;
+    derivativeWeight = DriveConstants.DERIVATIVE_WEIGHT;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(m_Recorder.getRobotData("joyX") == null || m_Recorder.getRobotData("joyY") == null ){
+    if(m_Recorder.getRobotData("joyX") == null 
+    || m_Recorder.getRobotData("joyY") == null 
+    || m_Recorder.getRobotData("leftEncoderDist") == null
+    || m_Recorder.getRobotData("leftEncoderDist") == null
+    ){
       return;
     }
     // get values from the recording
     joyX = m_Recorder.getRobotData("joyX");
-    joyY = m_Recorder.getRobotData("joyY");
-    //System.out.println(String.valueOf(joyX));
-    // place to adjust values
-    double forward = -1 * joyY;
-    double turn = joyX;	
+    joyY = -1 *m_Recorder.getRobotData("joyY");
+    leftEncoderDistRecorded = m_Recorder.getRobotData("leftEncoderDist");
+    rightEncoderDistRecorded = m_Recorder.getRobotData("rightEncoderDist");
+
+    // proportion = (what the distance should be - what the measured distance is) * weight
+    double leftProp = (leftEncoderDistRecorded - m_DriveTrain.leftEncoderDistance())*proportionWeight;
+    double rightProp = (rightEncoderDistRecorded - m_DriveTrain.rightEncoderDistance())*proportionWeight;
+
+    // derivative = proportion' aka velocity * weight
+    double leftDeriv = (m_DriveTrain.leftEncoderVelocity())*derivativeWeight;
+    double rightDeriv = (m_DriveTrain.rightEncoderVelocity())*derivativeWeight;
+
+    // convert arcade to left and right and add then correction throttle
+    double left  = (joyY + joyX) + (leftProp - leftDeriv)*PIDWeight;
+    double right = (joyY - joyX) + (rightProp - rightDeriv)*PIDWeight;	
     // drive train 
-    m_DriveTrain.arcadeDrive(forward, turn);
+    m_DriveTrain.tankDrive(left, right);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_DriveTrain.arcadeDrive(0, 0);
+    m_DriveTrain.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     // TODO: make this command end if safety values don't line up or something
-    return (m_Recorder.getRobotData("joyX") == null || m_Recorder.getRobotData("joyY") == null);
+    return (m_Recorder.getRobotData("joyX") == null 
+    || m_Recorder.getRobotData("joyY") == null 
+    || m_Recorder.getRobotData("leftEncoderDist") == null 
+    || m_Recorder.getRobotData("rightEncoderDist") == null );
   }
 }
