b49f4a56e62587ad7b6d2994998ec0c1fe2d5cae
Scott-McKool <44004555>
2021-03-28 11:01:00 -0400

    added reset gyro

---
+++ src/main/java/frc/robot/commands/Drive_Commands/BrainDeadAuton/ResetGyro.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Drive_Commands.BrainDeadAuton;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+public class ResetGyro extends CommandBase {
+  private DriveTrain _DriveTrain;
+  /** Creates a new ResetGyro. */
+  public ResetGyro(DriveTrain driveTrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    _DriveTrain = driveTrain;
+    addRequirements(_DriveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    _DriveTrain.resetGyro();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
